a4b50c7d1915c6d2bdac53110ec1d577e9a3ef1c
MauriceBonnesDev <maurice.bonnes@live.com>
2025-05-27 15:26:32 +0200

    Rotation works with a and d

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+from player import Player
 import pygame
 from constants import *
 
@@ -10,6 +11,9 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -17,7 +21,11 @@ def main():
                 return
 
         screen.fill(pygame.Color("black"))
+        player.draw(screen)
+        player.update(dt)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
